b3bfa47fa3c383ed46c6fad41262f39eeb39be44
Mrinal Devnath <78134020+mrinaldevnath@users.noreply.github.com>
2022-11-12 23:05:53 +0530

    Added error files

---
+++ src/components/pages/error/Error.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Error.scss'
+
+const Error = () => {
+    return (
+        <div className="container page_404">
+            <div className="row error-row">
+                <div className="col-sm-12 ">
+                    <div className=" col-sm-offset-1  text-center">
+                        <div className="four_zero_four_bg">
+                            <h1 className="text-center ">404</h1>
+
+
+                        </div>
+
+                        <div className="contant_box_404">
+                            <h3 className="h2">
+                                Look like you're lost
+                            </h3>
+
+                            <p>The page you are looking for is not available!</p>
+
+                            <a href="/" className="link_404">Go to Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Error
